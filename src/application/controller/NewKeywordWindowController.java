06b2038160218f09e20f07c3b38f0e57b9cdb6ef
hemaassen <helgemaassen@yahoo.de>
2017-03-02 16:59:01 +0100

    popup NewKeywordWindow.fxml und controller hinzugefügt

---
+++ src/application/controller/NewKeywordWindowController.java
@@ -0,0 +1,18 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewKeywordWindowController {
+	
+	@FXML
+    private TextField popupNewKeyword;
+
+    @FXML
+    private Button popupNewKeywordStop;
+
+    @FXML
+    private Button popupNewKeywordSave;
+
+}
